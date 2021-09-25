d92a3cfe513b34f546aa9d4c6ad74808e1e51d3c
orifmilod <orifmilod@gmail.com>
2021-09-25 15:35:41 +0200

    Added hovering

--- src/Board.cpp
+++ src/Board.cpp
@@ -2,22 +2,51 @@
 #include "Window.h"
 #include "utils/Logger.h"
 #include <SFML/System/Clock.hpp>
+#include <SFML/Window/Mouse.hpp>
 
 const float SIZE = 200;
 
+Board::Board() {
+  m_mouseClicked = false;
+  m_mouseDraging = false;
+}
+
+void Board::hover() {
+  if (m_event.type == sf::Event::MouseButtonPressed &&
+      m_event.mouseButton.button == sf::Mouse::Left) {
+    m_mouseClicked = true;
+  }
+}
+
+bool isRectClicked(sf::RectangleShape &shape) {
+  sf::IntRect rect(shape.getPosition().x, shape.getPosition().y,
+                   shape.getLocalBounds().width, shape.getLocalBounds().height);
+
+  if (rect.contains(sf::Mouse::getPosition()) &&
+      (sf::Mouse::isButtonPressed(sf::Mouse::Left))) {
+    return true;
+  }
+  return false;
+}
+
 void Board::render(Window &window) {
+  Logger::error(sf::Mouse::getPosition().x, " ", sf::Mouse::getPosition().y);
   sf::RectangleShape rectangle;
+  Logger::info(sf::Mouse::getPosition().x, " ", sf::Mouse::getPosition().y);
 
   // Default settings for each square
   rectangle.setSize({SIZE, SIZE});
 
-  // Draw the dark squares
+  // Draw the light squares
   for (float y = 0; y < 8; y++) {
     for (float x = 0; x < 4; x++) {
       float offset = int(y) & 1 ? SIZE : 0;
       rectangle.setPosition((SIZE * x * 2) + offset, SIZE * y);
       rectangle.setFillColor(sf::Color::White);
       window.Draw(rectangle);
+      if (isRectClicked(rectangle)) {
+        Logger::warn("y: ", y, "x: ", x, " clicked");
+      }
     }
   }
 
--- src/Board.h
+++ src/Board.h
@@ -1,7 +1,15 @@
 #pragma once
 #include "Window.h"
+#include <SFML/Window/Event.hpp>
 
 class Board {
+private:
+  sf::Event m_event;
+  bool m_mouseClicked;
+  bool m_mouseDraging;
+
 public:
+  Board();
   void render(Window &window);
+  void hover();
 };
