b4f902a08fcb6df84cceb2a007a17433a74c38a9
orifmilod <orifmilod@gmail.com>
2022-02-27 14:19:20 +0100

    Add Release and Debug build

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
 mkdir -p build && cd build
-cmake .. && make && ./bin/Chess
+echo $1 
+if [ "$1" == "Debug" ];
+then
+  echo "\033[0;32m  ============= Building debug version of the app ================"
+  cmake -DCMAKE_BUILD_TYPE=Debug -DFSANITIZE=address,undefined .. && make && ./bin/Chess
+else 
+  cmake -DCMAKE_BUILD_TYPE=Release  .. && make && ./bin/Chess
+fi
