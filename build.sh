22fe216267cf72dc309cdf2769d9316c6bcbb1ee
orifmilod <orifmilod@gmail.com>
2022-02-27 14:33:07 +0100

    Fix CMake issue

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ mkdir -p build && cd build
 echo $1 
 if [ "$1" == "Debug" ];
 then
-  echo "\033[0;32m  ============= Building debug version of the app ================"
+  echo "\033[0;32m===================== Building debug version of the app ========================"
   cmake -DCMAKE_BUILD_TYPE=Debug -DFSANITIZE=address,undefined .. && make && ./bin/Chess
 else 
   cmake -DCMAKE_BUILD_TYPE=Release  .. && make && ./bin/Chess
